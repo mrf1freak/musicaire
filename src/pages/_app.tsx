55a0f562b35e77445e572ca1d22e45396ce29c00
Saad <saadahmad96@hotmail.com>
2023-05-07 22:38:54 +0500

    feat: seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,21 @@ import { type AppType } from "next/app";
 import { api } from "~/trpc/api";
 
 import "~/styles/globals.css";
+import { DefaultSeo } from "next-seo";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo
+        title="Musicaire"
+        description="Discover new music for your library with our AI recommendations"
+        openGraph={{
+          images: [{ url: "/images/og.png", width: 1200, height: 630 }],
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
